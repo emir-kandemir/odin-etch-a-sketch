29ca2639eb466372472a04ec558b22f402b5f36f
emir-kandemir <227136149+emir-kandemir@users.noreply.github.com>
2026-06-01 19:21:04 +0300

    Refactor moved column and row variable to global scope

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+let column;
+let row;
+
 function createDiv(num) {
   for (let i = 0; i < num; i++) {
-    const column = document.createElement('div');
+    column = document.createElement('div');
     column.classList.add('column');
     gridLayout.appendChild(column);
     for (let j = 0; j < num; j++) {
-      const row = document.createElement('div');
+      row = document.createElement('div');
       row.classList.add('row');
       column.appendChild(row);
     }
@@ -14,3 +17,7 @@ function createDiv(num) {
 const container = document.querySelector('.container');
 const gridLayout = document.querySelector('.grid-layout');
 createDiv(16);
+
+column.addEventListener('mouseover',() => {
+  column.style.backgroundColor = 'red'
+})
